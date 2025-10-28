569ce8ff0a67173060470ffb64a6c82cf3255739
zatflz <timofeysangjey@gmail.com>
2025-10-28 14:22:14 +0300

    First commit and deploy

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const modal = document.getElementById('orderModal');
+const orderButtons = document.querySelectorAll('.btn-order');
+const closeModal = document.querySelector('.modal-close');
+
+orderButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        modal.style.display = 'block';
+    });
+});
+
+closeModal.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
